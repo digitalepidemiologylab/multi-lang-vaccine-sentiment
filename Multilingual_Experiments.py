4bb53c05ec07a3bd133ab34c137b6dd1ab01d579
Per Egil Kummervold <per@capia.no>
2020-02-08 16:08:47 +0100

    New refactored experiment file

--- Multilingual_Experiments.py
+++ Multilingual_Experiments.py
@@ -110,6 +110,49 @@ def get_run_config(output_dir):
         num_shards=NUM_TPU_CORES,
         per_host_input_for_training=tf.contrib.tpu.InputPipelineConfig.PER_HOST_V2))
 
+class vaccineStanceProcessor(run_classifier.DataProcessor):
+  """Processor for the NoRec data set."""
+
+  def get_train_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, 'train.tsv')), 'train')
+
+  def get_dev_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, 'dev.tsv')), 'dev')
+
+  def get_test_examples(self, data_dir):
+    """See base class."""
+    return self._create_examples(
+        self._read_tsv(os.path.join(data_dir, 'test.tsv')), 'test')
+
+  def get_labels(self):
+    """See base class."""
+    return ['positive','neutral','negative']
+
+  def _create_examples(self, lines, set_type):
+    """Creates examples for the training and dev sets."""
+    examples = []
+    for (i, line) in enumerate(lines):
+      # Only the test set has a header
+      if set_type == 'test' and i == 0:
+        continue
+      guid = '%s-%s' % (set_type, i)
+      if set_type == 'test':
+        text_a = tokenization.convert_to_unicode(line[3])
+        #Set a dummy value. This is not used
+        label = 'positive'
+      else:
+        text_a = tokenization.convert_to_unicode(line[3])
+        label = tokenization.convert_to_unicode(line[1])
+      examples.append(
+          run_classifier.InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
+    return examples
+
+
+
 def run_experiment(experiments):
     experiment_list = [x.strip() for x in experiments.split(',')]
     
--- vac_utils.py
+++ vac_utils.py
@@ -1,45 +1,4 @@
 #Define some custom functions
-class vaccineStanceProcessor(run_classifier.DataProcessor):
-  """Processor for the NoRec data set."""
-
-  def get_train_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, 'train.tsv')), 'train')
-
-  def get_dev_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, 'dev.tsv')), 'dev')
-
-  def get_test_examples(self, data_dir):
-    """See base class."""
-    return self._create_examples(
-        self._read_tsv(os.path.join(data_dir, 'test.tsv')), 'test')
-
-  def get_labels(self):
-    """See base class."""
-    return ['positive','neutral','negative']
-
-  def _create_examples(self, lines, set_type):
-    """Creates examples for the training and dev sets."""
-    examples = []
-    for (i, line) in enumerate(lines):
-      # Only the test set has a header
-      if set_type == 'test' and i == 0:
-        continue
-      guid = '%s-%s' % (set_type, i)
-      if set_type == 'test':
-        text_a = tokenization.convert_to_unicode(line[3])
-        #Set a dummy value. This is not used
-        label = 'positive'
-      else:
-        text_a = tokenization.convert_to_unicode(line[3])
-        label = tokenization.convert_to_unicode(line[1])
-      examples.append(
-          run_classifier.InputExample(guid=guid, text_a=text_a, text_b=None, label=label))
-    return examples
-
 
 def performance_metrics(y_true, y_pred, metrics=None, averaging=None, label_mapping=None):
     """
