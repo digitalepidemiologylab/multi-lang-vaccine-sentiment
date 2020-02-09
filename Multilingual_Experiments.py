2dcefb143cced3b43a837fbe268d30dae3bdc517
Per Egil Kummervold <per@capia.no>
2020-02-09 14:03:49 +0100

    New refactored experiment file

--- Multilingual_Experiments.py
+++ Multilingual_Experiments.py
@@ -66,7 +66,7 @@ import optimization
 import run_classifier
 import run_classifier_with_tfhub
 import tokenization
-
+d
 ##############################
 ########## CONSTANTS #########
 ##############################
@@ -171,22 +171,22 @@ experiment_definitions = {
     "2": {
         "name": "zeroshot--cb-annot-en--cb-annot-de",
         "train_annot_dataset": "cb-annot-en",
-        "eval_annot_dataset": "cb-annot-de"
+        "eval_annot_dataset": "cb-annot-en-de"
     },
     "3": {
         "name": "zeroshot--cb-annot-en--cb-annot-es",
         "train_annot_dataset": "cb-annot-en",
-        "eval_annot_dataset": "cb-annot-es"
+        "eval_annot_dataset": "cb-annot-en-es"
     },
     "4": {
         "name": "zeroshot--cb-annot-en--cb-annot-fr",
         "train_annot_dataset": "cb-annot-en",
-        "eval_annot_dataset": "cb-annot-fr"
+        "eval_annot_dataset": "cb-annot-en-fr"
     },
     "5": {
         "name": "zeroshot--cb-annot-en--cb-annot-pt",
         "train_annot_dataset": "cb-annot-en",
-        "eval_annot_dataset": "cb-annot-pt"
+        "eval_annot_dataset": "cb-annot-en-pt"
     },
     "6": {
         "name": "translate--cb-annot-en--cb-annot-en",
@@ -196,22 +196,22 @@ experiment_definitions = {
     "7": {
         "name": "translate--cb-annot-de--cb-annot-de",
         "train_annot_dataset": "cb-annot-de",
-        "eval_annot_dataset": "cb-annot-de"
+        "eval_annot_dataset": "cb-annot-en-de"
     },
     "8": {
         "name": "translate--cb-annot-es--cb-annot-es",
         "train_annot_dataset": "cb-annot-es",
-        "eval_annot_dataset": "cb-annot-es"
+        "eval_annot_dataset": "cb-annot-en-es"
     },
     "9": {
         "name": "translate--cb-annot-fr--cb-annot-fr",
         "train_annot_dataset": "cb-annot-fr",
-        "eval_annot_dataset": "cb-annot-fr"
+        "eval_annot_dataset": "cb-annot-en-fr"
     },
     "10": {
         "name": "translate--cb-annot-pt--cb-annot-pt",
         "train_annot_dataset": "cb-annot-pt",
-        "eval_annot_dataset": "cb-annot-pt"
+        "eval_annot_dataset": "cb-annot-en-pt"
     },
     "11": {
         "name": "multitranslate--cb-annot-en-de-fr-es",
@@ -221,22 +221,22 @@ experiment_definitions = {
     "12": {
         "name": "multitranslate--cb-annot-en-de-fr-es",
         "train_annot_dataset": "cb-annot-en-de-fr-es",
-        "eval_annot_dataset": "cb-annot-de"
+        "eval_annot_dataset": "cb-annot-en-de"
     },
     "13": {
         "name": "multitranslate--cb-annot-en-de-fr-es",
         "train_annot_dataset": "cb-annot-en-de-fr-es",
-        "eval_annot_dataset": "cb-annot-es"
+        "eval_annot_dataset": "cb-annot-en-es"
     },
     "14": {
         "name": "multitranslate--cb-annot-en-de-fr-es",
         "train_annot_dataset": "cb-annot-en-de-fr-es",
-        "eval_annot_dataset": "cb-annot-fr"
+        "eval_annot_dataset": "cb-annot-en-fr"
     },
     "15": {
         "name": "multitranslate--cb-annot-en-de-fr-es",
         "train_annot_dataset": "cb-annot-en-de-fr-es",
-        "eval_annot_dataset": "cb-annot-pt"
+        "eval_annot_dataset": "cb-annot-en-pt"
     }
 }
 
