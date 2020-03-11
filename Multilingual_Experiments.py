903b6422de600460d00d56600deacd5816791a1c
Per Egil Kummervold <peregil@Pers-iMac.local>
2020-03-11 12:08:33 +0100

    debug

--- Multilingual_Experiments.py
+++ Multilingual_Experiments.py
@@ -478,6 +478,12 @@ def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, u
                 is_training=False,
                 drop_remainder=False)
             predictions = estimator.predict(input_fn=train_pred_input_fn)
+
+            print(repr(predictions))
+            print(str(predictions))
+
+            sys.exit()
+
             probabilities = np.array([p['probabilities'] for p in predictions])
             y_true = [e.label_id for e in train_features]
 
--- vac_utils.py
+++ vac_utils.py
@@ -66,7 +66,7 @@ def get_predictions_output(experiment_id, guid, probabilities, y_true, label_map
         output[g].append({'prediction' : labels[0]})
         output[g].append({'predictions' : labels})
         output[g].append({'probability' : probabilities[i][sorted_ids][0]})
-        #output[g].append({'probabilities' : probabilities[i][sorted_ids]})
+        output[g].append({'probabilities' : probabilities[i][sorted_ids]})
         output[g].append({'y_true' : label_mapping[y_true[i]]})
     return output
 
