66751af36e2379153874d7e526d3653ffe7e9091
Martin Müller <martin.mathias.mueller@gmail.com>
2020-03-24 21:26:53 +0100

    hidden layer, cls extraction

--- Multilingual_Experiments.py
+++ Multilingual_Experiments.py
@@ -352,8 +352,7 @@ experiment_definitions = {
 ###########################
 
 
-def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, username,
-                   comment):
+def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, username, comment, store_last_layer):
     logger.info(f'Getting ready to run the following experiments for {repeat} repeats: {experiments}')
 
     def get_run_config(output_dir):
@@ -415,16 +414,19 @@ def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, u
             num_warmup_steps = int(num_train_steps * WARMUP_PROPORTION)
 
             #Initiation
-            model_fn = run_classifier.model_fn_builder(
-                bert_config=modeling.BertConfig.from_json_file(
-                    os.path.join(BERT_MODEL_DIR, 'bert_config.json')),
+
+            bert_config = modeling.BertConfig.from_json_file(os.path.join(BERT_MODEL_DIR, 'bert_config.json'))
+            model_fn = model_fn_builder(
+                bert_config=bert_config,
                 num_labels=len(label_list),
                 init_checkpoint=BERT_MODEL_FILE,
                 learning_rate=LEARNING_RATE,
                 num_train_steps=num_train_steps,
                 num_warmup_steps=num_warmup_steps,
                 use_tpu=use_tpu,
-                use_one_hot_embeddings=True)
+                use_one_hot_embeddings=True,
+                extract_last_layer=store_last_layer
+                )
 
             estimator = tf.contrib.tpu.TPUEstimator(
                 use_tpu=use_tpu,
@@ -468,11 +470,18 @@ def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, u
                 drop_remainder=False)
 
             predictions = estimator.predict(input_fn=train_pred_input_fn)
-            probabilities = np.array([p['probabilities'] for p in predictions])
+            probabilities, last_layer = list(zip(*[[p['probabilities'], p['last_layer']] for p in predictions]))
+            probabilities = np.array(probabilities)
+            if store_last_layer:
+                # extract state for CLS token 
+                last_layer = [_l[0] for _l in last_layer]
+            else:
+                last_layer = None
             y_true = [e.label_id for e in train_features]
             guid = [e.guid for e in train_examples]
-            predictions_output = get_predictions_output(experiment_id, guid, probabilities, y_true, label_mapping=label_mapping)
-            save_to_json(predictions_output ,os.path.join(PREDICTIONS_JSON_DIR, f'train_{experiment_id}.json'), dataset='train')
+            predictions_output = get_predictions_output(experiment_id, guid, probabilities, y_true, cls_hidden_state=last_layer, label_mapping=label_mapping)
+            save_to_json(predictions_output, os.path.join(PREDICTIONS_JSON_DIR, f'train_{experiment_id}.json'))
+
 
         #############################
         ######### EVALUATING ########
@@ -526,7 +535,7 @@ def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, u
 
         # write full dev prediction output
         predictions_output = get_predictions_output(experiment_id, guid, probabilities, y_true, label_mapping=label_mapping)
-        save_to_json(predictions_output, os.path.join(PREDICTIONS_JSON_DIR, f'dev_{experiment_id}.json'), dataset='dev')
+        save_to_json(predictions_output, os.path.join(PREDICTIONS_JSON_DIR, f'dev_{experiment_id}.json'))
 
         # Write log to Training Log File
         data = {
@@ -555,18 +564,124 @@ def run_experiment(experiments, use_tpu, tpu_address, repeat, num_train_steps, u
         logger.info("gsutil -m rm -r " + c)
         os.system("gsutil -m rm -r " + c)
 
+def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate, num_train_steps, num_warmup_steps, use_tpu, use_one_hot_embeddings, extract_last_layer=False):
+    """Returns `model_fn` closure for TPUEstimator."""
+    def model_fn(features, labels, mode, params):
+        """The `model_fn` for TPUEstimator."""
+        tf.logging.info("*** Features ***")
+        for name in sorted(features.keys()):
+            tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
+        input_ids = features["input_ids"]
+        input_mask = features["input_mask"]
+        segment_ids = features["segment_ids"]
+        label_ids = features["label_ids"]
+        is_real_example = None
+        if "is_real_example" in features:
+            is_real_example = tf.cast(features["is_real_example"], dtype=tf.float32)
+        else:
+            is_real_example = tf.ones(tf.shape(label_ids), dtype=tf.float32)
+        is_training = (mode == tf.estimator.ModeKeys.TRAIN)
+        model, (total_loss, per_example_loss, logits, probabilities) = create_model(
+                bert_config, is_training, input_ids, input_mask, segment_ids, label_ids,
+                num_labels, use_one_hot_embeddings)
+        tvars = tf.trainable_variables()
+        initialized_variable_names = {}
+        scaffold_fn = None
+        if init_checkpoint:
+            (assignment_map, initialized_variable_names
+            ) = modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+            if use_tpu:
+                def tpu_scaffold():
+                    tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+                    return tf.train.Scaffold()
+                scaffold_fn = tpu_scaffold
+            else:
+                tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+        tf.logging.info("**** Trainable Variables ****")
+        for var in tvars:
+            init_string = ""
+            if var.name in initialized_variable_names:
+                init_string = ", *INIT_FROM_CKPT*"
+            tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                                            init_string)
+        output_spec = None
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            train_op = optimization.create_optimizer(
+                    total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
+            output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+                    mode=mode,
+                    loss=total_loss,
+                    train_op=train_op,
+                    scaffold_fn=scaffold_fn)
+        elif mode == tf.estimator.ModeKeys.EVAL:
+            def metric_fn(per_example_loss, label_ids, logits, is_real_example):
+                predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
+                accuracy = tf.metrics.accuracy(
+                        labels=label_ids, predictions=predictions, weights=is_real_example)
+                loss = tf.metrics.mean(values=per_example_loss, weights=is_real_example)
+                return {"eval_accuracy": accuracy, "eval_loss": loss}
+            eval_metrics = (metric_fn, [per_example_loss, label_ids, logits, is_real_example])
+            output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+                    mode=mode,
+                    loss=total_loss,
+                    eval_metrics=eval_metrics,
+                    scaffold_fn=scaffold_fn)
+        else:
+            last_layer = None
+            if extract_last_layer:
+                last_layer = model.get_all_encoder_layers()[-1]
+            output_spec = tf.contrib.tpu.TPUEstimatorSpec(
+                    mode=mode,
+                    predictions={"probabilities": probabilities, 'last_layer': last_layer},
+                    scaffold_fn=scaffold_fn)
+        return output_spec
+    return model_fn
+
+
+def create_model(bert_config, is_training, input_ids, input_mask, segment_ids, labels, num_labels, use_one_hot_embeddings):
+    """Creates a classification model."""
+    model = modeling.BertModel(
+            config=bert_config,
+            is_training=is_training,
+            input_ids=input_ids,
+            input_mask=input_mask,
+            token_type_ids=segment_ids,
+            use_one_hot_embeddings=use_one_hot_embeddings)
+    output_layer = model.get_pooled_output()
+    hidden_size = output_layer.shape[-1].value
+    output_weights = tf.get_variable(
+            "output_weights", [num_labels, hidden_size],
+            initializer=tf.truncated_normal_initializer(stddev=0.02))
+    output_bias = tf.get_variable(
+            "output_bias", [num_labels], initializer=tf.zeros_initializer())
+    with tf.variable_scope("loss"):
+        if is_training:
+            # I.e., 0.1 dropout
+            output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+        logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+        logits = tf.nn.bias_add(logits, output_bias)
+        probabilities = tf.nn.softmax(logits, axis=-1)
+        log_probs = tf.nn.log_softmax(logits, axis=-1)
+        one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+        per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
+        loss = tf.reduce_mean(per_example_loss)
+    return model, (loss, per_example_loss, logits, probabilities)
+
+
 def parse_args(args):
     # Parse commandline
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tpu_ip",
-                        dest='tpu_ip',
-                        default=None,
-                        help="IP-address of the TPU")
-    parser.add_argument("--use_tpu",
-                        dest='use_tpu',
-                        action='store_true',
-                        default=False,
-                        help='Use TPU. Set to 1 or 0. If set to false, GPU will be used instead')
+    parser.add_argument(
+        '--tpu_ip',
+        dest='tpu_ip',
+        default=None,
+        help='IP-address of the TPU')
+    parser.add_argument(
+        '--use_tpu',
+        dest='use_tpu',
+        action='store_true',
+        default=False,
+        help='Use TPU. Set to 1 or 0. If set to false, GPU will be used instead')
     parser.add_argument(
         '-u',
         '--username',
@@ -586,11 +701,16 @@ def parse_args(args):
         help='Experiment number',
         default=['1'])
     parser.add_argument(
-            '-n',
-            '--num_train_steps',
-            help='Number of train steps. Default is 100',
-            default=100,
-            type=int)
+        '-n',
+        '--num_train_steps',
+        help='Number of train steps. Default is 100',
+        default=100,
+        type=int)
+    parser.add_argument(
+        '--store_last_layer',
+        action='store_true',
+        default=False,
+        help='Store last layer of encoder')
     parser.add_argument(
         '--comment',
         help='Optional. Add a Comment to the logfile for internal reference.',
@@ -605,15 +725,15 @@ def main(args):
     if args.use_tpu == 1:
         use_tpu = True
         tpu_address = tpu_init(args.tpu_ip)
-        logger.info("Using TPU")
+        logger.info('Using TPU')
     else:
         use_tpu = False
         tpu_address = None
-        logger.info("Using GPU")
+        logger.info('Using GPU')
 
     for repeat in range(args.repeats):
         run_experiment(args.experiments, use_tpu, tpu_address, repeat+1, args.num_train_steps,
-                       args.username, args.comment)
+                       args.username, args.comment, args.store_last_layer)
         logger.info(f'*** Completed repeats {repeat + 1}')
 
 
